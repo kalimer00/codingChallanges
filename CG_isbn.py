d015dd83717cbd69c2ba6a7f7862a51f8c8a37c6
kalimer00 <32965279+kalimer00@users.noreply.github.com>
2018-09-05 13:02:39 +0200

    Update CG_isbn.py

--- CG_isbn.py
+++ CG_isbn.py
@@ -1,67 +1,35 @@
+# coding=utf-8
 import sys
 import math
 import numpy as np
 
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
-n = int(input("Wieviele ISBN Nummer: "))
+n = int(input())
+
+
+def isbn_check(isbn, ref_isbn, multli, art_isbn):
+    arr_isbn = [int(i) for i in isbn]
+    arr_multiply = np.multiply(arr_isbn, ref_isbn)
+    sume = np.sum(arr_isbn)
+
+    print(arr_multiply)
+    print(sume)
+    print(ref_isbn)
+    print(arr_isbn)
+
 
 for i in range(n):
-    # I_isbn exemple 030640615
-    I_isbn = input("Eingabe der ISBN: ").strip()
-    arr_I_isbn_check = [10,9,8,7,6,5,4,3,2]
-    arr_II_isbn_check = [1,3,1,3,1,3,1,3,1,3,1,3]
-    arr_I_isbn = [int(i) for i in str(I_isbn)]
+    isbn = input()
+    # test: 030640615
+    ref_isbn_10 = [10, 9, 8, 7, 6, 5, 4, 3, 2]
+    # test: 978030640615
+    ref_isbn_13 = [1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3]
 
-    if len(arr_I_isbn) == 9:
-        I_isbn_mal = np.multiply(arr_I_isbn_check, arr_I_isbn)
-        I_isbn_sum = np.sum(I_isbn_mal)
-
-        I_isbn_rest = I_isbn_sum % 11
-        I_isbn_sum_new = I_isbn_sum
-
-        while I_isbn_rest > 0:
-            I_isbn_sum_new += 1
-            I_isbn_rest = I_isbn_sum_new % 11
-
-        I_isbn_digit = I_isbn_sum_new - I_isbn_sum
-        arr_I_isbn.insert(9, I_isbn_digit)
-        
-    elif len(arr_I_isbn) == 12:
-        # Testzahl 978030640615
-        II_isbn_mal = np.multiply(arr_II_isbn_check, arr_I_isbn)
-        II_isbn_sum = np.sum(II_isbn_mal)
-
-        II_isbn_rest = II_isbn_sum % 10
-        II_isbn_sum_new = II_isbn_sum
-
-        while II_isbn_rest > 0:
-            II_isbn_sum_new += 1
-            II_isbn_rest = II_isbn_sum_new % 10
-
-        II_isbn_digit = II_isbn_sum_new - II_isbn_sum
-        arr_I_isbn.insert(12, II_isbn_digit)
-
-    elif len(arr_I_isbn) >= 8 and len(arr_I_isbn) > 9:
-        print("Fuck you!")
+    if isbn.isdigit() and len(isbn) == 9:
+        isbn_check(isbn, ref_isbn_10, 11, 9)
+    else:
+        print(isbn)
 
 # Write an action using print
 # To debug: print("Debug messages...", file=sys.stderr)
-if len(arr_I_isbn) == 9:
-    print("ISBN: ", *arr_I_isbn)
-    print("Multiplikator: ", *arr_I_isbn_check)
-    print("Produkt der Arrays: ", *I_isbn_mal)
-    print("Summe mit Rest: ", I_isbn_sum)
-    print("Summe ohne Rest: ", I_isbn_sum_new)
-    print("End Zahl: ", I_isbn_digit)
-    
-elif len(arr_I_isbn) == 12:
-    print("ISBN: ", *arr_I_isbn)
-    print("Multiplikator: ", *arr_II_isbn_check)
-    print("Produkt der Arrays: ", *II_isbn_mal)
-    print("Summe mit Rest: ", II_isbn_sum)
-    print("Summe ohne Rest: ", II_isbn_sum_new)
-    print("End Zahl: ", II_isbn_digit)
-    
-elif len(arr_I_isbn) >= 8 and len(arr_I_isbn) > 9:
-    print("Gacke!")
